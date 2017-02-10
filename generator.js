072a6aa73d05417c3620c826ebd3a2f4c11913a4
jmeis <jmeis@us.ibm.com>
2017-02-10 15:56:11 -0600

    Updating to initial custom swagger doc

--- generator.js
+++ generator.js
@@ -84,7 +84,7 @@
 				console.log(app.text.green('    $ bluegen\n'));
 			}).then(function () {
 				var env = '  env: \n' +
-			'    "OPENAPI_SPEC": ' + '"/explorer/swagger.json"' + '\n';
+			'    "OPENAPI_SPEC": ' + '"/openapi/product.yml"' + '\n';
 
 				appendFile(path + '/manifest.yml', env);
 			}).catch(function (e) {
